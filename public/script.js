d5a0c940256eaf2a828ace0abab140fe5b61fc35
= <=>
2023-11-16 16:15:59 +0530

    made the code prettier

--- public/script.js
+++ public/script.js
@@ -1,14 +1,14 @@
 function validateForm() {
-    var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("confirmPassword").value;
-    var passwordError = document.getElementById("passwordError");
+  var password = document.getElementById("password").value;
+  var confirmPassword = document.getElementById("confirmPassword").value;
+  var passwordError = document.getElementById("passwordError");
 
-    if (password !== confirmPassword) {
-        passwordError.textContent = "Passwords do not match!";
-        return false;
-    } else {
-        //passwordError.textContent = "";
-        //console.log(true);
-        return true;
-    }
+  if (password !== confirmPassword) {
+    passwordError.textContent = "Passwords do not match!";
+    return false;
+  } else {
+    passwordError.textContent = "";
+    console.log(true);
+    return true;
+  }
 }
