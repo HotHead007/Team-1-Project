ac90ab97d7ac4226f8f082e70f4c310ba605d956
MOHAN <mrcoder187@gmail.com>
2023-11-16 14:02:33 +0530

    created form with basic styles and js functunality

---
+++ public/script.js
@@ -0,0 +1,14 @@
+function validateForm() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    var passwordError = document.getElementById("passwordError");
+
+    if (password !== confirmPassword) {
+        passwordError.textContent = "Passwords do not match!";
+        return false;
+    } else {
+        //passwordError.textContent = "";
+        //console.log(true);
+        return true;
+    }
+}
